52df3a1fbc08edd20dd322bef2027570ee03004a
Nick <nvoss@me.com>
2024-12-01 20:03:49 -0600

    Day 1 cleanup #2

--- src/main/java/InputReader.java
+++ src/main/java/InputReader.java
@@ -1,42 +1,26 @@
 package main.java;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class InputReader {
-    private static final Logger logger = Logger.getLogger(InputReader.class.getName());
-    private String[] inputLines;
-    private final AdventDate date;
-    private final boolean useTestfile;
+    private final String[] inputLines;
 
     public InputReader(AdventDate date, boolean useTestfile) {
-        var classloader = Thread.currentThread().getContextClassLoader();
+        final String path = "aoc-" + date.year() + "-" + date.day();
+        final String fileName = useTestfile ? path + "-test" + ".txt" : path + ".txt";
 
-        this.date = date;
-        this.useTestfile = useTestfile;
+        try {
+            var resource = this.getClass().getClassLoader().getResource(fileName);
+            var file = Files.readAllLines(Paths.get(resource.toURI()));
 
-        final String path = "aoc-" + this.date.year() + "-" + this.date.day();
-        final String fileName = this.useTestfile ? path + "-test" + ".txt" : path + ".txt";
-
-        try (var inputStream = classloader.getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                logger.log(Level.SEVERE, "File not found: " + fileName);
-                return;
-            }
-
-            try (Scanner scanner = new Scanner(inputStream)) {
-                var linesList = new ArrayList<String>();
-                while (scanner.hasNextLine()) {
-                    linesList.add(scanner.nextLine());
-                }
-                inputLines = linesList.toArray(new String[0]);
-            }
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error reading the file.", e);
+            inputLines = file.toArray(new String[0]);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     public String[] getInputLines() {
