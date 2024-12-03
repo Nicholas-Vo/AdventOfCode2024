4fa8ef95ce90b6e692c1adf3df1c56c0f559abab
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2024-12-03 16:43:55 +0000

    day3part2

--- src/main/java/y24/Day03.java
+++ src/main/java/y24/Day03.java
@@ -39,7 +39,57 @@ public class Day03 {
         });
 
         day3.doAnswer(2, () -> {
-            return 1;
+            var functionList = new ArrayList<String>();
+            Pattern pattern = Pattern.compile("mul\\(\\d*,\\d*\\)|do\\(\\)|don't\\(\\)", Pattern.CASE_INSENSITIVE);
+
+            for (String line : inputLines) {
+                Matcher matcher = pattern.matcher(line);
+
+                while (matcher.find()) {
+                    // store each function string; ex. "mul(25, 135)"
+                    functionList.add(matcher.group());
+                }
+            }
+
+            var sum = 0;
+            var enabled = true;
+
+            // mul(25, 135)
+            // don't()
+            // mul(15, 236)
+            // do()
+            // mul(5, 532)
+
+            for (String function : functionList) {
+                if (function.contains("don't")) {
+                    enabled = false;
+                    continue;
+                }
+
+                if (function.contains("do()")) {
+                    enabled = true;
+                    continue;
+                }
+
+                if (enabled) {
+                    sum += calculate(function);
+                }
+            }
+
+            return sum;
         });
+    }
+
+    private static long calculate(String function) {
+        if (!function.startsWith("mul")) {
+            return 0L;
+        }
+        // parse out to an array of elements; ex. "[25, 135]"
+        String[] nums = function
+                .replace("mul(", "")
+                .replace(")", "")
+                .split((","));
+
+        return Integer.decode(nums[0]) * Integer.decode(nums[1]);
     }
 }
