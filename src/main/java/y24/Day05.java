8bf7e283718d06e1154af42c74e5ada11752cb06
Nick <nvoss@me.com>
2024-12-07 19:00:59 -0600

    Day 5 part 2

--- src/main/java/y24/Day05.java
+++ src/main/java/y24/Day05.java
@@ -2,11 +2,7 @@ package main.java.y24;
 
 import main.java.AdventDay;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Day05 {
     private static final Map<Integer, Set<Integer>> ruleMap = new HashMap<>();
@@ -21,36 +17,77 @@ public class Day05 {
         String[] ruleList = rules.split("\n");
 
         day5.doAnswer(1, () -> {
-            for (String rule : ruleList) {
-                int[] ruleArray = Arrays.stream(rule.split("\\|"))
-                        .map(s -> s.trim())
-                        .mapToInt(s -> Integer.parseInt(s))
-                        .toArray();
-
-                int keyPage = ruleArray[0];
-                int valPage = ruleArray[1];
-
-                ruleMap.putIfAbsent(keyPage, new HashSet<>(valPage));
-
-                Set<Integer> pageSet = ruleMap.get(keyPage);
-                pageSet.add(valPage);
-            }
-
             int sum = 0;
-            String[] updateList = updates.trim().split("\n");
-            for (String update : updateList) {
+            for (String update : getUpdateList(ruleList, updates)) {
                 int[] pages = Arrays.stream(update.split(","))
                         .map(s -> s.trim())
                         .mapToInt(s -> Integer.parseInt(s))
                         .toArray();
 
                 if (isValidUpdate(pages)) {
-                    sum += /* middle number: */ pages[pages.length / 2];
+                    sum += /* middle number */ pages[pages.length / 2];
                 }
             }
 
             return sum;
         });
+
+        day5.doAnswer(2, () -> {
+            int sum = 0;
+            for (String update : getUpdateList(ruleList, updates)) {
+                int[] pages = Arrays.stream(update.split(","))
+                        .map(String::trim)
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+                if (!isValidUpdate(pages)) {
+                    int[] sorted = sortPages(pages);
+                    sum += sorted[sorted.length / 2];
+                }
+            }
+
+            return sum;
+        });
+    }
+
+    private static int[] sortPages(int[] pages) {
+        Integer[] sortedPages = Arrays.stream(pages).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(sortedPages, (a, b) -> {
+            var disallowedA = ruleMap.get(a);
+            var disallowedB = ruleMap.get(b);
+
+            // b must come after a
+            if (disallowedA.contains(b)) {
+                return 1;
+            }
+            // a must come after b
+            if (disallowedB.contains(a)) {
+                return -1;
+            }
+            return 0;
+        });
+
+        return Arrays.stream(sortedPages).mapToInt(i -> i).toArray();
+    }
+
+    private static String[] getUpdateList(String[] ruleList, String updates) {
+        for (String rule : ruleList) {
+            int[] ruleArray = Arrays.stream(rule.split("\\|"))
+                    .map(s -> s.trim())
+                    .mapToInt(s -> Integer.parseInt(s))
+                    .toArray();
+
+            int keyPage = ruleArray[0];
+            int valPage = ruleArray[1];
+
+            ruleMap.putIfAbsent(keyPage, new HashSet<>(valPage));
+
+            Set<Integer> pageSet = ruleMap.get(keyPage);
+            pageSet.add(valPage);
+        }
+
+        return updates.trim().split("\n");
     }
 
     // rule = 47|53
